b2d4944c9ed963552a339781d659eca9f9a22ae1
Akhishka-0 <100042292+Akhishka-0@users.noreply.github.com>
2025-04-05 14:02:23 +0300

    Add files via upload

---
+++ Лабараторная_2.1/Лабараторная_2.1/Лабараторная_2.1.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+
+class Character {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Character(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Character " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Character() {
+        std::cout << "Character " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Monster {
+private:
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+public:
+    // Конструктор
+    Monster(const std::string& n, int h, int a, int d)
+        : name(n), health(h), attack(a), defense(d) {
+        std::cout << "Monster " << name << " created!\n";
+    }
+
+    // Деструктор
+    ~Monster() {
+        std::cout << "Monster " << name << " destroyed!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", HP: " << health
+            << ", Attack: " << attack << ", Defense: " << defense << std::endl;
+    }
+};
+
+class Weapon {
+private:
+    std::string name;
+    int damage;
+    float mass;
+
+public:
+    Weapon(const std::string& n, int d, float m) : name(n), damage(d), mass(m) {
+        std::cout << "Weapon " << name << " created!\n";
+    }
+
+    ~Weapon() {
+        std::cout << "Weapon " << name << " destroyer!\n";
+    }
+
+    void displayInfo() const {
+        std::cout << "Name: " << name << ", Damage: " << damage << ", Mass: " << mass;
+    }
+};
+
+void main() {
+    Weapon Ak47("AK-47", 300, 3.2);
+    Weapon M416("M416", 250, 2.5);
+    Weapon AWP("AWP", 500, 5);
+}
